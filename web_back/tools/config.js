04d1c37282680dee53ac5f4fc2e6cc3a3845fb2f
张琳凯 <1241726505@qq.com>
2019-12-15 16:54:10 +0800

    第一天功能

---
+++ web_back/tools/config.js
@@ -0,0 +1,3 @@
+var BASE_URL = "http://localhost:8000";
+var LOGIN = BASE_URL + "/admin/login"
+var LOGOUT = BASE_URL + "/admin/logout"
---
+++ web_back/tools/user.js
@@ -0,0 +1,66 @@
+// 对项目中与用户操作有关的请求
+var user = {
+
+
+
+    login: function (options) {
+
+        $.ajax({
+            type: "post",
+            url: LOGIN,
+            data: {
+                user_name: options.data.user_name,
+                password: options.data.password
+
+
+            },
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+
+                }
+                else {
+                    options.fail();
+
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+
+        })
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
